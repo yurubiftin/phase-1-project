740e8959df28d549a2fade56dccabde3326ee267
yurubahmed <yurubiftin2002@gmail.com>
2022-12-15 13:33:52 -0500

    added function getClassByRate

--- index.js
+++ index.js
@@ -37,3 +37,12 @@ function showMovies(movies){
     main.appendChild(movieEl);
     });
 }
+function getClassByRate(vote){
+    if(vote >=8){
+        return"green";
+    }else if(vote >=5){
+        return"orange";
+    }else{
+        return"red";
+    }
+}
